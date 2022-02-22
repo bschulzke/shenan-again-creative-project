36bb8392612f6fc87fb8663f1d2f2c69015dc0c7
Ben Schulzke <benschulzke@gmail.com>
2022-02-21 19:14:27 -0700

    email validation API, responsive styling. STABLE.

--- javascript.js
+++ javascript.js
@@ -47,3 +47,47 @@ function toggleToolsMenuIcon() {
     }
     toolsIconVisible = !toolsIconVisible;
 }
+
+function isEmpty(element) {
+    return element === null || element === undefined || element === NaN || element === "";
+}
+
+function sendEmail() {
+    if (!isEmpty($('#emailAddress').val()) && !isEmpty($('#message-body').val())) {
+    document.getElementById("emailAddress").value = "";
+    document.getElementById("message-body").value = "";
+    document.getElementById("email-success").style = "color: green";
+    document.getElementById("email-success").innerText = "Email submitted!";   
+    }
+    else if (isEmpty($('#emailAddress').val())) {
+        document.getElementById("email-success").style = "color: #CF6679";
+        document.getElementById("email-success").innerText = "enter an email address";
+    } else {
+        document.getElementById("email-success").style = "color: #CF6679";
+        document.getElementById("email-success").innerText = "enter a message";
+    }
+}
+
+function validateEmail() {
+    document.getElementById("emailAddress").addEventListener("keyup", function(event) {
+        event.preventDefault();
+        });
+        document.getElementById("emailAddress").addEventListener("keyup", function(event) {
+        event.preventDefault();
+        const url = "https://api.eva.pingutil.com/email?email="+
+          document.getElementById("emailAddress").value;
+        fetch(url)
+            .then(function(response) {
+                return response.json();
+            }).then(function(json) {
+                if (json.status == 'success') {
+                    console.log('Success!');
+                    document.getElementById("txtHint").innerHTML=
+                    `<span></span>` 
+                } else {
+                    document.getElementById("txtHint").innerHTML=
+                    `<span style="color: #CF6679">enter a valid email addesss</span>` 
+                }
+            });
+    });
+}
