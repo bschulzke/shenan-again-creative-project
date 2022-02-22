c6a3c53d7f044038816ce429dc2c7701508a6dac
Ben Schulzke <benschulzke@gmail.com>
2022-02-21 17:52:30 -0700

    js: stable Calculator, TODO: styling

--- calculator.js
+++ calculator.js
@@ -21,9 +21,35 @@ let probsOnSix = [
 ]
 
 function calculate() {
-    console.log("Probablitiy " + probsOnSix[$('#dice-selector').val()]);
+    if (isEmpty($('#dice-selector').val())) {
+        console.log("empty dice selector");
+    }
+    else if ($('#dice-selector').val() <= 18) {
+        document.getElementById("prob-results").innerText = ("Probablitiy: " + probsOnSix[$('#dice-selector').val()] + "%");
+    } else {
+        document.getElementById("prob-results").innerText = "Probablitiy: 99%";
+    }
 }
 
-function logChange() {
-    console.log($('#dice-selector').val());
+function addDice() {
+    let value = 0;
+    if (!isEmpty($('#dice-selector').val())) {
+        value = parseInt($('#dice-selector').val()) + 1;
+    }
+        document.getElementById("dice-selector").value = value;
+        document.getElementById("dice-selector").innerText = value;
+    calculate();
+}
+
+function subtractDice() {
+    if (parseInt($('#dice-selector').val()) > 0) {
+        let value = parseInt($('#dice-selector').val()) - 1;
+        document.getElementById("dice-selector").value = value;
+        document.getElementById("dice-selector").innerText = value;
+    }
+    calculate();
+}
+
+function isEmpty(element) {
+    return element === null || element === undefined || element === NaN || element === "";
 }
