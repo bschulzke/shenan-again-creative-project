6e029ec57fa7463220fafc01f013d18a1273df99
Ben Schulzke <benschulzke@gmail.com>
2022-02-26 12:54:07 -0700

    reset negatives to zero on calculator, rounded corner on tools menu

--- js/calculator.js
+++ js/calculator.js
@@ -28,7 +28,11 @@ function calculate() {
         console.log("empty dice selector");
         return;
     }
-    else if ($('#dice-selector').val() <= 21) {
+    if ($('#dice-selector').val() < 0) {
+        document.getElementById("dice-selector").value = 0;
+        document.getElementById("dice-selector").innerText = 0;
+    }
+    if ($('#dice-selector').val() <= 21) {
         document.getElementById("prob-results").innerText = (probsOnSix[$('#dice-selector').val()] 
         + "% chance of rolling at least one six on " + $('#dice-selector').val() + " dice");
     } else {
