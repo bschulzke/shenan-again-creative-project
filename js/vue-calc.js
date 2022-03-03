38b323946b426a9156d14e70fc8e9ce823007ef8
Ben Schulzke <benschulzke@gmail.com>
2022-03-03 11:56:36 -0700

    choose target number of dice

--- js/vue-calc.js
+++ js/vue-calc.js
@@ -3,67 +3,41 @@ let calculator = new Vue({
     data: {
         numDice: 1,
         targets: 'six',
-        probsOnSix: [
-            0,
-            17,
-            31,
-            42,
-            52,
-            60,
-            67,
-            72,
-            77,
-            81,
-            84,
-            87,
-            88,
-            90,
-            92,
-            94,
-            94,
-            95,
-            96,
-            97,
-            97,
-            98
-        ],
-        probsOnFiveOrSix: [
-            0,
-            31,
-            52,
-            67,
-            77,
-            84,
-            89,
-            92,
-            95,
-            96,
-            97,
-            99,
-            99,
-            99,
-            99,
-            99,
-            99,
-            99,
-        ],
+        six: [0,17,31,42,52,60,67,72,77,81,84,87,88,90,92,94,94,95,96,97,97,98],
+        fiveOrSix: [0,31,52,67,77,84,89,92,95,96,97,99,99,99,99,99,99,99,],
+        fourFiveOrSix: [0,42,67,81,89,94,96,98,99,99,99.6,],
     },
     computed: {
         percentChance() {
             if (this.targets === 'six') {
                 if (this.numDice < 22) {
-                    return this.probsOnSix[this.numDice];
+                    return this.six[this.numDice];
                 } else {
                     return 99;
                 }
             } else if (this.targets === 'fiveOrSix') {
                 if (this.numDice < 18) {
-                    return this.probsOnFiveOrSix[this.numDice];
+                    return this.fiveOrSix[this.numDice];
+                } else {
+                    return 99.99;
+                } 
+            } else {
+                if (this.numDice < 11) {
+                    return this.fourFiveOrSix[this.numDice];
                 } else {
                     return 99.99;
                 }
             }
         },
+        targetPhrase() {
+            if (this.targets === 'six') {
+                return "six";
+            } else if (this.targets === 'fiveOrSix') {
+                return "five or six";
+            } else if (this.targets === 'fourFiveOrSix') {
+                return "four, five or six";
+            }
+        }
     },
     methods: {
         addDice() {
